59d8968c9b0fdea8b254f64cca769d8b9e772397
UTKARSH SINGH <usjadon19@gmail.com>
2024-06-01 22:53:49 +0530

    flutter

---
+++ lib/main_development.dart
@@ -0,0 +1,8 @@
+import 'package:xcrescent_me_flutter/app/app.dart';
+import 'package:xcrescent_me_flutter/bootstrap.dart';
+
+/// This entry point should be used for development only
+void main() {
+  ///You can override your environment variable in bootstrap method here for providers
+  bootstrap(() => const App());
+}
